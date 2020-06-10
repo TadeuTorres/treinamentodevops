f0d8b7afcd89690e5bb6d76f0c9486afa4df4c10
guto.cbn@hotmail.com <guto.cbn@hotmail.com>
2020-06-10 05:11:49 -0300

    dia 7

--- Dia 7/Laboratório 7/demo1/Protótipo.cs
+++ Dia 7/Laboratório 7/demo1/Protótipo.cs
@@ -1,13 +1,12 @@
-﻿
-            var numbers = new List<int>();
-            var random = new Random();
+﻿var numbers = new List<int>();
+var random = new Random();
 
-            while(numbers.Count < 6)
-            {
-                var number = random.Next(1, 60);
-                numbers.Add(number);
-            }
+while(numbers.Count < 6)
+{
+    var number = random.Next(1, 60);
+    numbers.Add(number);
+}
 
-            numbers.ForEach(n => Console.WriteLine($"{n} "));
+numbers.ForEach(n => Console.WriteLine($"{n} "));
 
-            Console.ReadLine();
+Console.ReadLine();
